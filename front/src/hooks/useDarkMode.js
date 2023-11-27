94ce872fc7274340ac9ead39f5f4a6fc3d749813
Kagistan EESHVARAN <eeshvarankagistan@gmail.com>
2023-11-27 14:58:25 +0100

    default light mode

--- front/src/hooks/useDarkMode.js
+++ front/src/hooks/useDarkMode.js
@@ -1,16 +1,16 @@
-import { useEffect } from 'react';
-import useLocalStorage from './useLocalStorage';
+import { useEffect } from "react";
+import useLocalStorage from "./useLocalStorage";
 
 /**
  * Hook to manage dark mode
  * @returns {Array} [theme, setTheme]
  */
 const useDarkMode = () => {
-  const [theme, setTheme] = useLocalStorage('theme', 'dark');
+  const [theme, setTheme] = useLocalStorage("theme", "light");
 
   useEffect(() => {
     const html = window.document.documentElement;
-    html.setAttribute('data-theme', theme);
+    html.setAttribute("data-theme", theme);
   }, [theme]);
 
   return [theme, setTheme];
